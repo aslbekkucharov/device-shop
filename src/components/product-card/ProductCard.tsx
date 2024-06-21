e1580ac4a78d3a22a94d3a47f9508b095e40b9bb
Kucharov Aslbek <kuas@corp.muk.ua>
2024-06-21 18:16:58 +0500

    style: add price label for product card

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from '@/utils'
 import { Product } from '@/types/product'
 import classes from '@/components/product-card/product-card.module.scss'
 
@@ -8,7 +9,10 @@ interface Props {
 export default function ProductCard(props: Props) {
     return (
         <span className={classes['product-card']}>
-            <span className={classes['product-card__title']}>{props.data.name}</span>
+            <span className={classes['product-card__head']}>
+                <span className={classes['product-card__title']}>{props.data.name}</span>
+                <span className={classes['product-card__price']}>{formatPrice(props.data.price)}</span>
+            </span>
             <span className={classes['product-card__excerpt']}>{props.data.description}</span>
         </span>
     )
